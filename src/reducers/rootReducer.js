dfa437bcc69f7136143bf0d5b0b2bb288f98bd6b
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2023-03-19 16:07:30 +0300

    cart synchronization with local storage added

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,6 +3,16 @@ import storeReducer from "./storeReducer";
 import sortFormReducer from "./sortFormReducer";
 import cartReducer from "./cartReducer";
 
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
 const rootReducer = combineReducers({store: storeReducer, sortForm: sortFormReducer, cart: cartReducer})
 
-export default rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default persistedReducer
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers/rootReducer'
+import { persistStore } from 'redux-persist'
 
 const store = createStore(rootReducer)
+
+const persistor = persistStore(store)
+
+export {persistor}
 
 export default store
